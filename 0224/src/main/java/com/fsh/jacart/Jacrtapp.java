ee400986d98635109b2a3b92e581aa9bce2963d8
fsh <15731098022@163.com>
2020-02-24 19:05:14 +0800

    update admin

---
+++ 0224/src/main/java/com/fsh/jacart/Jacrtapp.java
@@ -0,0 +1,9 @@
+package com.fsh.jacart;
+
+/**
+ * @author Mr.Blake
+ * @create 2020-02-24 19:01
+ */
+public class Jacrtapp {
+
+}
---
+++ 0224/src/main/java/com/fsh/jacart/controller/AdministratorController.java
@@ -0,0 +1,14 @@
+package com.fsh.jacart.controller;
+
+/**
+ * @author Mr.Blake
+ * @create 2020-02-24 19:02
+ */
+@RestController
+@RequestMapping("/administrator")
+public class AdministratorController {
+    @GetMapping("/login")
+    public String login(){
+        return null;
+    }
+}
