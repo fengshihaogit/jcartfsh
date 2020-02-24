3b6ec500e689a7614a74e37a058eb91f20c99f7e
fsh <15731098022@163.com>
2020-02-24 20:42:00 +0800

    first init administration back

--- 0224/src/main/java/com/fsh/jacart/Jacrtapp.java
+++
@@ -1,9 +0,0 @@
-package com.fsh.jacart;
-
-/**
- * @author Mr.Blake
- * @create 2020-02-24 19:01
- */
-public class Jacrtapp {
-
-}
---
+++ jcart-administration-back/src/test/java/com/fsh/jcartadministrationback/JcartAdministrationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsh.jcartadministrationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartAdministrationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
