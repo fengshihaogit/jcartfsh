08e6d38572a453476af03d3e4661fc08bac221f6
fsh <15731098022@163.com>
2020-02-25 22:14:58 +0800

    draft product apis

---
+++ jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/controller/ProductController.java
@@ -0,0 +1,52 @@
+package com.fsh.jcartadministrationback.controller;
+
+import com.fsh.jcartadministrationback.dto.in.ProductCreateInDTO;
+import com.fsh.jcartadministrationback.dto.in.ProductSearchInDTO;
+import com.fsh.jcartadministrationback.dto.in.ProductUpdateInDTO;
+import com.fsh.jcartadministrationback.dto.out.PageOutDTO;
+import com.fsh.jcartadministrationback.dto.out.ProductListOutDTO;
+import com.fsh.jcartadministrationback.dto.out.ProductShowOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Mr.Blake
+ * @create 2020-02-25 20:49
+ */
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/search")
+    public PageOutDTO<ProductListOutDTO> search(ProductSearchInDTO productSearchInDTO,
+                                                @RequestParam Integer pageNum){
+        return null;
+    }
+
+    @GetMapping("/getById")
+    public ProductShowOutDTO getById(@RequestParam Integer productId){
+        return null;
+    }
+
+    @PostMapping("/create")
+    public Integer create (@RequestBody ProductCreateInDTO productCreateInDTO){
+        return null;
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody ProductUpdateInDTO productUpdateInDTO){
+
+    }
+
+    @PostMapping("/delete")
+    public void delete(@RequestBody Integer productId){
+
+    }
+
+    @PostMapping("/batchDelete")
+    public void batchDelete(@RequestBody List<Integer> productIds){
+
+    }
+
+}
---
+++ jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/dto/in/ProductSearchInDTO.java
@@ -0,0 +1,54 @@
+package com.fsh.jcartadministrationback.dto.in;
+
+/**
+ * @author Mr.Blake
+ * @create 2020-02-25 20:54
+ */
+public class ProductSearchInDTO {
+
+    private String productCode;
+    private String productName;
+    private Double price;
+    private Integer stockQuantity;
+    private Byte status;
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+}
