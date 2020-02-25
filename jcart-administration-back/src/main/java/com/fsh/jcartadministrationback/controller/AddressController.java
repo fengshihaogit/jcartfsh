10ead5b307e5b0fbb5c1a3bd92f0bc86e9799f54
fsh <15731098022@163.com>
2020-02-25 22:38:36 +0800

    add address api

--- jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/controller/AddressController.java
+++ jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/controller/AddressController.java
@@ -1,8 +1,24 @@
 package com.fsh.jcartadministrationback.controller;
 
+import com.fsh.jcartadministrationback.dto.out.AddressListOutDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @author Mr.Blake
  * @create 2020-02-25 22:33
  */
+@RestController
+@RequestMapping("/address")
 public class AddressController {
+
+    @GetMapping("/getListByCustomerId")
+    public List<AddressListOutDTO> getListByCustomerId(
+            @RequestParam Integer customerId){
+        return null;
+    }
 }
--- jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/dto/out/AddressListOutDTO.java
+++ jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/dto/out/AddressListOutDTO.java
@@ -5,4 +5,49 @@ package com.fsh.jcartadministrationback.dto.out;
  * @create 2020-02-25 22:34
  */
 public class AddressListOutDTO {
-   
+    private Integer addressId;
+    private String receiverName;
+    private String receiverMobile;
+    private String content;
+    private String tag;
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getReceiverMobile() {
+        return receiverMobile;
+    }
+
+    public void setReceiverMobile(String receiverMobile) {
+        this.receiverMobile = receiverMobile;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
