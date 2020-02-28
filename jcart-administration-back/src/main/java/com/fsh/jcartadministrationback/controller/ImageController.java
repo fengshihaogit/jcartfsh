0fd48595b681b6dd99a066175427f4819118318b
fsh <15731098022@163.com>
2020-02-28 14:23:58 +0800

    add administration umage upload api

--- jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/controller/ImageController.java
+++ jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/controller/ImageController.java
@@ -1,10 +1,18 @@
 package com.fsh.jcartadministrationback.controller;
 
+import com.fsh.jcartadministrationback.constant.ClientExceptionConstant;
+import com.fsh.jcartadministrationback.exception.ClientException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Mr.Blake
@@ -14,8 +22,28 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/image")
 public class ImageController {
 
+    private List<String> imageExts= Arrays.asList("jpg","jpeg","png");
+
     @PostMapping("/upload")
-    public String upload(@RequestParam MultipartFile image){
-        return null;
+    public String upload(@RequestParam MultipartFile image) throws ClientException {
+            String originalFilename = image.getOriginalFilename();
+            String[] splits = originalFilename.split("\\.");
+            String ext = splits[splits.length - 1];
+            ext = ext.toLowerCase();
+            //todo judge with content type
+            boolean contains = imageExts.contains(ext);
+            if (!contains){
+                throw new ClientException(ClientExceptionConstant.IMAGE_INVALID_ERRCODE, ClientExceptionConstant.IMAGE_INVALID_ERRMSG);
+            }
+            String uuid = UUID.randomUUID().toString();
+            String filename = String.format("%s.%s", uuid, ext);
+            String filepath = String.format("www/image/%s", filename);
+            try(FileOutputStream out = new FileOutputStream(filepath)){
+                byte[] data = image.getBytes();
+                out.write(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return filename;
+        }
     }
-}
