9bcd298b9c65436dec895f5cba1730905f3425d5
fsh <15731098022@163.com>
2020-02-28 14:32:30 +0800

    add administration image upload api

--- jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/controller/ImageController.java
+++ jcart-administration-back/src/main/java/com/fsh/jcartadministrationback/controller/ImageController.java
@@ -1,10 +1,18 @@
 package com.fsh.jcartadministrationback.controller;
 
+import com.fsh.jcartadministrationback.constant.ClientExceptionConstant;
+import com.fsh.jcartadministrationback.exception.ClientException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Mr.Blake
