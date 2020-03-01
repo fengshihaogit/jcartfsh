8d634482db70a02fe1e998630bade5fcf0199ec9
fsh <15731098022@163.com>
2020-03-01 14:15:41 +0800

    add store product search page

--- jcart-store-back/src/main/java/com/fsh/jcartstoreback/controller/ProductController.java
+++ jcart-store-back/src/main/java/com/fsh/jcartstoreback/controller/ProductController.java
@@ -8,13 +8,11 @@ import com.fsh.jcartstoreback.dto.out.ProductShowOutDTO;
 import com.fsh.jcartstoreback.service.ProductService;
 import com.github.pagehelper.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
+@CrossOrigin
 public class ProductController {
 
     @Autowired
---
+++ jcart-store-front/viewmodels/product-search.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    pageInfo: '',
+    PageNum: 1
+  },
+  mounted() {
+    console.log('view mounted');
+    this.searchProduct();
+  },
+  methods: {
+    handlePageChange(val) {
+      console.log('page change', val);
+      this.pageNum = val;
+      this.searchProduct();
+    },
+    searchProduct() {
+      axios.get('/product/search', {
+        params: {
+          pageNum: this.pageNum
+        }
+      })
+        .then(function (response) {
+          console.log(response);
+          app.pageInfo = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+  }
+})
