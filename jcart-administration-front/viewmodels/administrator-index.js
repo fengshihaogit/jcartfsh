de745a1ed9a4fdd56f78c849b32ecc7b11a64b9b
fsh <15731098022@163.com>
2020-03-05 22:42:06 +0800

    add administration administrator delete page

--- jcart-administration-front/viewmodels/administrator-index.js
+++ jcart-administration-front/viewmodels/administrator-index.js
@@ -16,6 +16,28 @@ var app = new Vue({
             this.pageNum = val;
             this.getAdministrators();
         },
+        handleDelete(index,row){
+            console.log('delete click')
+
+            if(confirm('确认删除？')){
+                this.deleteAdministrator(row.administratorId);
+            }
+        },
+        deleteAdministrator(administratorId){
+            axios.post('/administrator/delete',administratorId, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+              .then(function (response) {
+                console.log(response);
+                alert('删除成功')
+                location.reload();
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        },
         getAdministrators() {
             axios.get('/administrator/getList', {
                 params: {
