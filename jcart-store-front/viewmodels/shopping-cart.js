a4dc4a3b5ae7f3eeb77efd411ab044ddda82b382
fsh <15731098022@163.com>
2020-03-06 22:31:19 +0800

    draft store shopping cart page update

--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -10,10 +10,16 @@ var app = new Vue({
         this.myShoppingCart = JSON.parse(myShoppingCartJson);
     },
     methods: {
+        handleUpdate(){
+            console.log('update click')
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+            this.$message.success('修改购物车成功')
+        },
         handleDelete(index, row) {
             console.log('delete click')
             this.myShoppingCart.splice(index, 1);
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+            this.$message.success('删除购物车成功')
         }
     }
 })
