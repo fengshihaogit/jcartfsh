19df4c86d0973799e244630e4db2601c160254f4
fsh <15731098022@163.com>
2020-03-11 18:44:54 +0800

    add store return show page

--- jcart-store-front/viewmodels/return-show.js
+++ jcart-store-front/viewmodels/return-show.js
@@ -1,85 +1,62 @@
 var app = new Vue({
     el: '#app',
     data: {
+        returnId: '',
         orderId: '',
-        orderTime: '',
+        orderTimestamp: '',
         customerName: '',
         mobile: '',
         email: '',
-        productId: '',
+        status: '',
+        action: '',
         productCode: '',
         productName: '',
         quantity: '',
         reason: '',
-        comment: '',
         opened: '',
-    },
-    computed: {
-        orderTimestamp() {
-            return this.orderTime.getTime();
-        }
+        comment: '',
+        createTimestamp: '',
+        updateTimestamp: '',
+        returnHistories: []
     },
     mounted() {
         console.log('view mounted');
 
         var url = new URL(location.href);
-        this.orderId = url.searchParams.get("orderId");
-        if (!this.orderId) {
-            alert('orderId is null');
+        this.returnId = url.searchParams.get("returnId");
+        if (!this.returnId) {
+            alert('returnId is null');
             return;
         }
 
-        this.productId = url.searchParams.get("productId");
-        if (!this.productId) {
-            alert('productId is null');
-            return;
-        }
-
-        this.getOrderById();
-
+        this.getReturnById();
     },
     methods: {
-        handleApplyClick() {
-            console.log('apply click');
-            this.applyReturn();
-        },
-        getOrderById() {
-            axios.get('/order/getById', {
+        getReturnById() {
+            axios.get('/return/getById', {
                 params: {
-                    orderId: this.orderId
+                    returnId: this.returnId
                 }
             })
                 .then(function (response) {
                     console.log(response);
-                    var order = response.data;
-                    app.orderTime = new Date(order.createTimestamp);
-                    var orderProducts = order.orderProducts;
-                    var returnProduct = orderProducts.find(p => p.productId == app.productId);
-                    app.productCode = returnProduct.productCode;
-                    app.productName = returnProduct.productName;
-                    app.quantity = returnProduct.quantity;
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        },
-        applyReturn() {
-            axios.post('/return/apply', {
-                orderId: this.orderId,
-                orderTimestamp: this.orderTimestamp,
-                customerName: this.customerName,
-                mobile: this.mobile,
-                email: this.email,
-                productCode: this.productCode,
-                productName: this.productName,
-                quantity: this.quantity,
-                reason: this.reason,
-                opened: this.opened,
-                comment: this.comment
-            })
-                .then(function (response) {
-                    console.log(response);
-                    alert('申请成功，请等待处理');
+                    var aReturn = response.data;
+                    app.orderId = aReturn.orderId;
+                    app.orderTimestamp = aReturn.orderTimestamp;
+                    app.customerName = aReturn.customerName;
+                    app.mobile = aReturn.mobile;
+                    app.email = aReturn.email;
+                    app.status = aReturn.status;
+                    app.action = aReturn.action;
+                    app.productCode = aReturn.productCode;
+                    app.productName = aReturn.productName;
+                    app.quantity = aReturn.quantity;
+                    app.reason = aReturn.reason;
+                    app.opened = aReturn.opened;
+                    app.comment = aReturn.comment;
+                    app.createTimestamp = aReturn.createTimestamp;
+                    app.updateTimestamp = aReturn.updateTimestamp;
+                    app.returnHistories = aReturn.returnHistories;
                 })
                 .catch(function (error) {
                     console.log(error);
