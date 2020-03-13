ebc8670e95e602ebd2e26362b35ad800381dd2e2
fsh <15731098022@163.com>
2020-03-13 17:32:33 +0800

    add administration product search page

--- jcart-administration-front/viewmodels/product-search.js
+++ jcart-administration-front/viewmodels/product-search.js
@@ -2,13 +2,36 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        productCode: '',
+        productName: '',
+        price: '',
+        stockQuantity: '',
+        selectedStatus: '',
+        statuses: [
+            { value: 0, label: '下架' },
+            { value: 1, label: '上架' },
+            { value: 2, label: '待审核' }
+        ]
     },
     mounted(){
         console.log("view  mounted");
         this.searchProduct();
     },
     methods:{
+        handleSearchClick(){
+            console.log('search click')
+            this.pageNum = 1;
+            this.searchProduct();
+        },
+        handleClearClick(){
+            console.log('clear click')
+            this.productCode = '';
+            this.productName = '';
+            this.price = '';
+            this.stockQuantity = '';
+            this.selectedStatus = '';
+        },
         handlePageChange(val){
             console.log("page change");
             this.pageNum = val;
@@ -17,6 +40,11 @@ var app = new Vue({
         searchProduct(){
             axios.get('/product/search', {
                 params: {
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    price: this.price,
+                    stockQuantity: this.stockQuantity,
+                    status: this.selectedStatus,
                     pageNum: this.pageNum
                 }
               })
