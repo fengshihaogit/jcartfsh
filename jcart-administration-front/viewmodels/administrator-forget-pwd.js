7f6fffc5d25b55a30114de19976e3e87db30a04c
fsh <15731098022@163.com>
2020-03-20 21:15:32 +0800

    add count down

--- jcart-administration-front/viewmodels/administrator-forget-pwd.js
+++ jcart-administration-front/viewmodels/administrator-forget-pwd.js
@@ -2,12 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         email: '',
-        loading: false
+        loading: false,
+        buttonEnabled : true,
+        counter : 60
     },
     methods: {
         handleFindBackPwdClick() {
             console.log('find back pwd click');
             this.loading = true;
+            this.buttonEnabled = false;
+
+            setInterval(function () {
+                console.loading('count down')
+                app.counter--;
+            },1000);
+
             this.getPwdResetCode();
         },
         getPwdResetCode() {
@@ -19,7 +28,7 @@ var app = new Vue({
                 .then(function (response) {
                     console.log(response);
                     app.loading = false;
-                    alert('重置码已发送到邮箱');
+                    // alert('重置码已发送到邮箱');
                 })
                 .catch(function (error) {
                     console.log(error);
