5b1f4785151331a57ff624cbe9318354ad3d6e11
fsh <fengshihao_888@163.com>
2020-03-23 21:12:30 +0800

    first init support service

---
+++ jcart-support-back/src/main/java/com/fsh/jcartsupportback/JcartSupportBackApplication.java
@@ -0,0 +1,13 @@
+package com.fsh.jcartsupportback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartSupportBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartSupportBackApplication.class, args);
+    }
+
+}
---
+++ jcart-support-back/src/test/java/com/fsh/jcartsupportback/JcartSupportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsh.jcartsupportback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartSupportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
